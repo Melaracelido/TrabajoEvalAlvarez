678a467e2e0ac8086332563c91455ce6cad3812c
zanahoria6 <lopezruiztomas59@gmail.com>
2024-05-30 10:53:17 -0300

    terminamos el registro y comenzamos con el inicio de sesion

--- src/app/modules/autentificacion/pages/registro/registro.component.ts
+++ src/app/modules/autentificacion/pages/registro/registro.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { EmailValidator } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 import { Usuario } from 'src/app/models/usuario'
 
 @Component({
@@ -18,4 +20,25 @@ export class RegistroComponent {
     rol: '',
     password: '',
   }
+
+// coleccion de usuarios
+coleccionusuarios: Usuario[] = [];
+//registro
+Registrar(){
+  const credenciales = {
+    uid: this.usuarios.uid,
+    nombre: this.usuarios.uid,
+    apellido: this.usuarios.apellido,
+    email: this.usuarios.email,
+    rol: this.usuarios.rol,
+    password: this.usuarios.password,
+
+  }
+  //enviamos los nuevos registros 
+  this.coleccionusuarios.push(credenciales);
+
+  // por consola
+  console.log(credenciales);
+  console.log(this.coleccionusuarios)
+}
 }
